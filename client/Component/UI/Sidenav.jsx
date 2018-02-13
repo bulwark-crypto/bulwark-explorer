56c99dfc71be2f4f4aacc89c29b26b659771c9ae
Dustin Engle <dustin@dustinengle.com>
2018-02-13 01:29:51 -0800

    added links to sidenav

--- client/Component/UI/Sidenav.jsx
+++ client/Component/UI/Sidenav.jsx
@@ -1,5 +1,6 @@
 
 import Component from '../../Core/Component';
+import { Nav, NavItem, NavLink } from 'reactstrap';
 import PropTypes from 'prop-types';
 import React from 'react';
 
@@ -11,7 +12,39 @@ export default class UISidenav extends Component {
     render() {
         return (
             <nav className={ `sidenav ${ this.props.isOpen ? 'open' : 'close'}` }>
-                Sidenav
+                <h6>MENU</h6>
+                <Nav vertical>
+                    <NavItem>
+                        <NavLink className="active" href="#">
+                            <i className="material-icons">home</i> Overview
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">
+                            <i className="material-icons">equalizer</i> Transactions
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">
+                            <i className="material-icons">star</i> Network
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">
+                            <i className="material-icons">group</i> Top 100
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">
+                            <i className="material-icons">assessment</i> Statistics
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">
+                            <i className="material-icons">info</i> API
+                        </NavLink>
+                    </NavItem>
+                </Nav>
             </nav>
         );
     };
