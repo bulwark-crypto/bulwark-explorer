db5555bb512b88ba7300dcb66d9adcdecb45f5f7
Allan Smith <asmith@tagrem.com>
2018-02-20 07:48:48 -0800

    initial table commit

--- client/Component/Table.jsx
+++ client/Component/Table.jsx
@@ -4,24 +4,84 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 export default class Table extends Component {
-    render() {
+  static defaultProps = {
+    cols: [],
+    data: [],
+  };
+
+  static propTypes = {
+    cols: PropTypes.array,
+    data: PropTypes.array,
+    max: PropTypes.number,
+  };
+
+  componentDidMount() {
+  };
+
+  componentWillUnmount() {
+  };
+
+  getHeader() {
+    const keys = this.getKeys();
+
+    const cells = keys.map((col, idx) => {
+      return (
+        <th key={ idx }>{ col }</th>
+      )
+    });
+
+    return (
+      <thead>
+        <tr>
+          { cells }
+        </tr>
+      </thead>
+    );
+  }
+
+  getBody() {
+    const { data } = this.props;
+    const keys = this.getKeys();
+
+    const rows = data.map((row, idx) => {
+      const cells = keys.map((col, i) => {
         return (
-            <table>
-                <thead>
-                    <tr>
-                        <th>One</th>
-                        <th>Two</th>
-                        <th>Three</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>A</td>
-                        <td>B</td>
-                        <td>C</td>
-                    </tr>
-                </tbody>
-            </table>
-        );
-    };
+          <td key={ i }>{ row[col] }</td>
+        )
+      });
+
+      return (
+        <tr key={ idx }>
+          { cells }
+        </tr>
+      )
+    })
+
+    return (
+      <tbody>
+        { rows }
+      </tbody>
+    );
+  }
+
+  getKeys() {
+    const { cols } = this.props;
+
+    if (typeof col === 'object') {
+      // TODO allow for different titles than the object key
+    }
+
+    return [ ...cols ];
+  }
+
+  render() {
+    const { cols, data } = this.props;
+
+    return (
+      <table>
+        {this.getHeader()}
+        {this.getBody()}
+      </table>
+    );
+  };
 }
--- client/Container/Overview.jsx
+++ client/Container/Overview.jsx
@@ -1,11 +1,37 @@
 
 import Component from '../Core/Component';
 import React from 'react';
+import Table from '../Component/Table';
 
 export default class Overview extends Component {
-    render() {
-        return (
-            <h1>Overview</h1>
-        );
-    };
+  state = {
+    cols: ['height', 'hash', 'age', 'amount', 'recipients', 'time'],
+    data: [
+      {height: 70500, hash: '5891564sdaf456sdf5621dfs3sd5afd89a76asd2f1a3ew8597sdfa564sa1fsa3', age: 1, amount: 1, recipients: 1, time: Date.now()},
+      {height: 70500, hash: '5891564sdaf456sdf5621dfs3sd5afd89a76asd2f1a3ew8597sdfa564sa1fsa3', age: 1, amount: 1, recipients: 1, time: Date.now()},
+      {height: 70500, hash: '5891564sdaf456sdf5621dfs3sd5afd89a76asd2f1a3ew8597sdfa564sa1fsa3', age: 1, amount: 1, recipients: 1, time: Date.now()},
+      {height: 70500, hash: '5891564sdaf456sdf5621dfs3sd5afd89a76asd2f1a3ew8597sdfa564sa1fsa3', age: 1, amount: 1, recipients: 1, time: Date.now()},
+      {height: 70500, hash: '5891564sdaf456sdf5621dfs3sd5afd89a76asd2f1a3ew8597sdfa564sa1fsa3', age: 1, amount: 1, recipients: 1, time: Date.now()},
+      {height: 70500, hash: '5891564sdaf456sdf5621dfs3sd5afd89a76asd2f1a3ew8597sdfa564sa1fsa3', age: 1, amount: 1, recipients: 1, time: Date.now()},
+      {height: 70500, hash: '5891564sdaf456sdf5621dfs3sd5afd89a76asd2f1a3ew8597sdfa564sa1fsa3', age: 1, amount: 1, recipients: 1, time: Date.now()},
+      {height: 70500, hash: '5891564sdaf456sdf5621dfs3sd5afd89a76asd2f1a3ew8597sdfa564sa1fsa3', age: 1, amount: 1, recipients: 1, time: Date.now()},
+      {height: 70500, hash: '5891564sdaf456sdf5621dfs3sd5afd89a76asd2f1a3ew8597sdfa564sa1fsa3', age: 1, amount: 1, recipients: 1, time: Date.now()},
+      {height: 70500, hash: '5891564sdaf456sdf5621dfs3sd5afd89a76asd2f1a3ew8597sdfa564sa1fsa3', age: 1, amount: 1, recipients: 1, time: Date.now()},
+      {height: 70500, hash: '5891564sdaf456sdf5621dfs3sd5afd89a76asd2f1a3ew8597sdfa564sa1fsa3', age: 1, amount: 1, recipients: 1, time: Date.now()},
+      {height: 70500, hash: '5891564sdaf456sdf5621dfs3sd5afd89a76asd2f1a3ew8597sdfa564sa1fsa3', age: 1, amount: 1, recipients: 1, time: Date.now()},
+      {height: 70500, hash: '5891564sdaf456sdf5621dfs3sd5afd89a76asd2f1a3ew8597sdfa564sa1fsa3', age: 1, amount: 1, recipients: 1, time: Date.now()},
+      {height: 70500, hash: '5891564sdaf456sdf5621dfs3sd5afd89a76asd2f1a3ew8597sdfa564sa1fsa3', age: 1, amount: 1, recipients: 1, time: Date.now()},
+    ]
+  }
+
+  render() {
+    const { cols, data } = this.state;
+
+    return (
+      <div>
+        <h1>Overview</h1>
+        <Table cols={ cols } data={ data } />
+      </div>
+    );
+  };
 }
