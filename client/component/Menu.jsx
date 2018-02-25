ab5378f8b3dbbbace55e933cf7138ce48557864f
Allan Smith <asmith@tagrem.com>
2018-02-25 11:28:36 -0800

    fixes missed files in rename

---
+++ client/component/Menu.jsx
@@ -0,0 +1,57 @@
+
+import Component from '../core/Component';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Icon from './Icon';
+
+export default class Menu extends Component {
+  static defaultProps = {
+    isOpen: true,
+  };
+
+  static propTypes = {
+    isOpen: PropTypes.bool,
+    handleToggle: PropTypes.func,
+  };
+
+  render() {
+    const { props } = this;
+
+    return (
+      <div className={ `menu ${ props.isOpen ? 'menu--open' : 'menu--close' }` }>
+        <div className="menu__wrapper">
+          <div className="menu__header">
+            <img src="/img/logo.jpg" className="menu__logo" />
+            <a onClick={ props.handleToggle } >
+              <Icon name="menu" className="menu__toggle" onClick={ props.handleToggle } />
+            </a>
+          </div>
+          <p className="menu__title">MENU</p>
+            <Link className="menu__item" to="/">
+              <Icon name="home" className="menu__icon" /> <span className="menu__item-label" >Overview</span>
+            </Link>
+            <Link className="menu__item" to="/movement">
+              <Icon name="home" className="menu__icon" /> <span className="menu__item-label" >Movement</span>
+            </Link>
+            <Link className="menu__item" to="/top">
+              <Icon name="home" className="menu__icon" /> <span className="menu__item-label">Top 100</span>
+            </Link>
+            <Link className="menu__item" to="/masternode">
+              <Icon name="home" className="menu__icon" /> <span className="menu__item-label">Masternode</span>
+            </Link>
+            <Link className="menu__item" to="/coin">
+              <Icon name="home" className="menu__icon" /> <span className="menu__item-label" >Coin Info</span>
+            </Link>
+            <Link className="menu__item" to="/faq">
+              <Icon name="home" className="menu__icon" /> <span className="menu__item-label">FAQ</span>
+            </Link>
+            <Link className="menu__item" to="/api">
+              <Icon name="home" className="menu__icon" /> <span className="menu__item-label">API</span>
+            </Link>
+          </div>
+      </div>
+    )
+  }
+}
---
+++ client/core/Component.jsx
@@ -0,0 +1,4 @@
+
+import React from 'react';
+
+export default class Component extends React.Component {}
