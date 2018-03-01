7b4b035242c1357808d81d860597c9e9b4c33770
Allan Smith <asmith@tagrem.com>
2018-02-28 20:43:35 -0800

    updates footer styles

--- client/component/Footer.jsx
+++ client/component/Footer.jsx
@@ -25,37 +25,49 @@ class Footer extends Component {
     return (
       <div className="footer">
         <div className="footer__wrapper">
-            <div className="footer__block">
-              <img src="https://bulwarkcrypto.com/wp-content/uploads/2018/01/bulwark_footer.png" className="footer__logo" />
+            <div className="footer__data-wrapper">
+                <img src="https://bulwarkcrypto.com/wp-content/uploads/2018/01/bulwark_footer.png" className="footer__logo" />
+                <span className="footer__data-block footer__legal">
+                  <p>Copyright &copy; 2018 Bulwark Cryptocurrency</p>
+                  <p>Site design / Logo &copy; 2018 Bulwark Cryptocurrency</p>
+                  <p>User contributions licensed under cc by-sa 3.0 with attribution required</p>
+                </span>
             </div>
-            <div className="footer__block">
-              <div className="footer__legal">
-                <p>Copyright &copy; 2018 Bulwark Cryptocurrency</p>
-                <p>Site design / Logo &copy; 2018 Bulwark Cryptocurrency</p>
-                <p>User contributions licensed under cc by-sa 3.0 with attribution required</p>
-              </div>
+            <div className="footer__data-wrapper">
+                <div className="footer__data-block">
+                  <p className="footer__title">Status</p>
+                  <p>
+                    <span className={ `u__dot u--text-${ statusColor }` }>&bull;</span>
+                    <span className="footer__title">{ coin.status }</span>
+                  </p>
+                </div>
+                <div className="footer__data-block">
+                  <p className="footer__title">Blocks</p>
+                  <p><b>{ coin.blocks }</b></p>
+                </div>
             </div>
-            <div className="footer__block">
-              <div>
-                <p className="footer__title">Status</p>
-                <p>
-                  <span className={ `u__dot u--text-${ statusColor }` }>&bull;</span>
-                  <span className="footer__title">{ coin.status }</span>
-                </p>
-              </div>
-              <div>
-                <p className="footer__title">Blocks</p>
-                <p><b>{ coin.blocks }</b></p>
-              </div>
-            </div>
-            <div className="footer__block">
+            <div className="footer__data-block">
               <p className="footer__title">Social Media</p>
               <div className="footer__social-media-wrapper">
-                <Icon name="envelope-square" className="footer__social-media-icon" />
-                <Icon name="reddit" className="footer__social-media-icon" />
-                <Icon name="github" className="footer__social-media-icon" />
-                <Icon name="twitter" className="footer__social-media-icon" />
-                <Icon name="facebook" className="footer__social-media-icon" />
+                <a href="#">
+                  <Icon name="envelope-square" className="footer__social-media-icon" />
+                </a>
+                <a href="#">
+                  <Icon name="reddit" className="
+                  fab footer__social-media-icon" />
+                </a>
+                <a href="#">
+                  <Icon name="github" className="
+                  fab footer__social-media-icon" />
+                </a>
+                <a href="#">
+                  <Icon name="twitter" className="
+                  fab footer__social-media-icon" />
+                </a>
+                <a href="#">
+                  <Icon name="facebook" className="
+                  fab footer__social-media-icon" />
+                </a>
               </div>
             </div>
         </div>
