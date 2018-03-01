3a9690d639d6fe1e33244329a811a4fd3f29f8bf
Allan Smith <asmith@tagrem.com>
2018-02-28 22:15:54 -0800

    adds initial coininfo component

--- client/container/CoinInfo.jsx
+++ client/container/CoinInfo.jsx
@@ -1,11 +1,31 @@
 
 import Component from '../core/Component';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import React from 'react';
 
-export default class CoinInfo extends Component {
+import CoinSummary from 'component/CoinSummary';
+import HorizontalRule from 'component/HorizontalRule';
+
+class CoinInfo extends Component {
   render() {
     return (
-      <h1>CoinInfo</h1>
+      <div>
+        <CoinSummary />
+        <HorizontalRule title="Coin Info" />
+        <img src="https://bulwarkcrypto.com/wp-content/uploads/2018/01/Badge-Asset-9.png" />
+      </div>
     );
   };
 }
+
+const mapDispatch = dispatch => ({
+  clear: () => dispatch({ payload: [], type: TXS }),
+  getLatest: query => Actions.getTXLatest(dispatch, query)
+});
+
+const mapState = state => ({
+  txs: state.txs
+});
+
+export default connect(mapState, mapDispatch)(CoinInfo);
