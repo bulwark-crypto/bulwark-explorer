cb968c93fdd844b934a1007272a726ff1e857a14
Allan Smith <asmith@tagrem.com>
2018-03-07 22:47:07 -0800

    Merge

--- client/component/Menu/MenuDesktop.jsx
+++ client/component/Menu/MenuDesktop.jsx
@@ -22,47 +22,47 @@ export default class MenuDesktop extends Component {
     const { state } = this;
 
     return (
-      <div>
-        <div className={ `menu-desktop ${ state.isOpen ? 'menu-desktop--open' : 'menu-desktop--close' }` }>
-          <div className="menu-desktop__wrapper">
-            <div className="menu-desktop__header">
-              <img src="/img/Badge-White.png" className="menu-desktop__logo" />
-              <a onClick={ this.handleToggle } >
-                <Icon name="bars" className="menu-desktop__toggle" onClick={ this.handleToggle } />
-              </a>
-            </div>
-            <p className="menu-desktop__title">MENU</p>
-              <Link className="menu-desktop__item" to="/">
-                <img className="menu-desktop__icon" src="/img/Home-icon@2x.png" />
-                <span className="menu-desktop__item-label active" >Overview</span>
-              </Link>
-              <Link className="menu-desktop__item" to="/movement">
-                <img className="menu-desktop__icon" src="/img/Analytics-icon@2x.png" />
-                <span className="menu-desktop__item-label" >Movement</span>
-              </Link>
-              <Link className="menu-desktop__item" to="/top">
-                <img className="menu-desktop__icon" src="/img/Ratings-icon@2x.png" />
-                <span className="menu-desktop__item-label">Top 100</span>
-              </Link>
-              <Link className="menu-desktop__item" to="/masternode">
-                <img className="menu-desktop__icon" src="/img/Customers-icon@2x.png" />
-                <span className="menu-desktop__item-label">Masternode</span>
-              </Link>
-              <Link className="menu-desktop__item" to="/coin">
-                <img className="menu-desktop__icon" src="/img/Market-icon@2x.png" />
-                <span className="menu-desktop__item-label" >Coin Info</span>
-              </Link>
-              {/*
-              <Link className="menu-desktop__item" to="/faq">
-                <img className="menu-desktop__icon" src="/img/Jobs-icon@2x.png" />
-                <span className="menu-desktop__item-label">FAQ</span>
-              </Link>
-              */}
-              <Link className="menu-desktop__item" to="/api">
-                <img className="menu-desktop__icon" src="/img/Support-icon@2x.png" />
-                <span className="menu-desktop__item-label">API</span>
-              </Link>
-            </div>
+      <div className={ `menu-desktop ${ state.isOpen ? 'menu-desktop--open' : 'menu-desktop--close' }` }>
+        <div className="menu-desktop__content-wrapper">
+          <div className="menu-desktop__header">
+            <img src="/img/Badge-White.png" className="menu-desktop__logo" />
+            <a onClick={ this.handleToggle } >
+              <Icon name="bars" className="menu-desktop__toggle" onClick={ this.handleToggle } />
+            </a>
+          </div>
+          <p className="menu-desktop__title">MENU</p>
+          <Link className="menu-desktop__item" to="/">
+            <img className="menu-desktop__icon" src="/img/Home-icon@2x.png" />
+            <span className="menu-desktop__item-label" >Overview</span>
+          </Link>
+          <Link className="menu-desktop__item" to="/movement">
+            <img className="menu-desktop__icon" src="/img/Analytics-icon@2x.png" />
+            <span className="menu-desktop__item-label" >Movement</span>
+          </Link>
+          {/*
+          <Link className="menu-desktop__item" to="/top">
+            <img className="menu-desktop__icon" src="/img/Ratings-icon@2x.png" />
+            <span className="menu-desktop__item-label">Top 100</span>
+          </Link>
+          */}
+          <Link className="menu-desktop__item" to="/masternode">
+            <img className="menu-desktop__icon" src="/img/Customers-icon@2x.png" />
+            <span className="menu-desktop__item-label">Masternode</span>
+          </Link>
+          <Link className="menu-desktop__item" to="/coin">
+            <img className="menu-desktop__icon" src="/img/Market-icon@2x.png" />
+            <span className="menu-desktop__item-label" >Coin Info</span>
+          </Link>
+          {/*
+          <Link className="menu-desktop__item" to="/faq">
+            <img className="menu-desktop__icon" src="/img/Jobs-icon@2x.png" />
+            <span className="menu-desktop__item-label">FAQ</span>
+          </Link>
+          */}
+          <Link className="menu-desktop__item" to="/api">
+            <img className="menu-desktop__icon" src="/img/Support-icon@2x.png" />
+            <span className="menu-desktop__item-label">API</span>
+          </Link>
         </div>
       </div>
     )
--- client/component/Menu/MenuMobile.jsx
+++ client/component/Menu/MenuMobile.jsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
 import Icon from '../Icon';
+import SearchBar from '../SearchBar';
 
 export default class MenuDesktop extends Component {
   constructor(props) {
@@ -22,48 +23,21 @@ export default class MenuDesktop extends Component {
     const { state } = this;
 
     return (
-      <div>
-        <div className={ `menu-mobile ${ state.isOpen ? 'menu-mobile--open' : 'menu-mobile--close' }` }>
-          <div className="menu-mobile__wrapper">
-            <div className="menu-mobile__header">
-              <img src="/img/Badge-White.png" className="menu-mobile__logo" />
-              <a onClick={ this.handleToggle } >
-                <Icon name="bars" className="menu-mobile__toggle" onClick={ this.handleToggle } />
-              </a>
-            </div>
-            <p className="menu-mobile__title">MENU</p>
-              <Link className="menu-mobile__item" to="/">
-                <img className="menu-mobile__icon" src="/img/Home-icon@2x.png" />
-                <span className="menu-mobile__item-label active" >Overview</span>
-              </Link>
-              <Link className="menu-mobile__item" to="/movement">
-                <img className="menu-mobile__icon" src="/img/Analytics-icon@2x.png" />
-                <span className="menu-mobile__item-label" >Movement</span>
-              </Link>
-              <Link className="menu-mobile__item" to="/top">
-                <img className="menu-mobile__icon" src="/img/Ratings-icon@2x.png" />
-                <span className="menu-mobile__item-label">Top 100</span>
-              </Link>
-              <Link className="menu-mobile__item" to="/masternode">
-                <img className="menu-mobile__icon" src="/img/Customers-icon@2x.png" />
-                <span className="menu-mobile__item-label">Masternode</span>
-              </Link>
-              <Link className="menu-mobile__item" to="/coin">
-                <img className="menu-mobile__icon" src="/img/Market-icon@2x.png" />
-                <span className="menu-mobile__item-label" >Coin Info</span>
-              </Link>
-              {/*
-              <Link className="menu-mobile__item" to="/faq">
-                <img className="menu-mobile__icon" src="/img/Jobs-icon@2x.png" />
-                <span className="menu-mobile__item-label">FAQ</span>
-              </Link>
-              */}
-              <Link className="menu-mobile__item" to="/api">
-                <img className="menu-mobile__icon" src="/img/Support-icon@2x.png" />
-                <span className="menu-mobile__item-label">API</span>
-              </Link>
-            </div>
-        </div>
+      <div className="menu-mobile">
+        <Navbar>
+          <SearchBar />
+          <NavbarToggler onClick={ this.handleToggle } className="mr-2" />
+          <Collapse isOpen={ !state.isOpen } navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
       </div>
     )
   }
