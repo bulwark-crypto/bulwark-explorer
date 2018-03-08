b89ec45e87a4d81aaff3e7cd83c0a861d6578908
Dustin Engle <dustin@dustinengle.com>
2018-03-07 20:50:57 -0800

    changed to mobile class for mobile menu to view

--- client/component/Menu/MenuMobile.jsx
+++ client/component/Menu/MenuMobile.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 
 import Icon from '../Icon';
 
-export default class MenuDesktop extends Component {
+export default class MenuMobile extends Component {
   static defaultProps = {
     isOpen: true,
   };
@@ -21,46 +21,46 @@ export default class MenuDesktop extends Component {
 
     return (
       <div>
-        <div className={ `menu-desktop ${ props.isOpen ? 'menu-desktop--open' : 'menu-desktop--close' }` }>
-          <div className="menu-desktop__wrapper">
-            <div className="menu-desktop__header">
-              <img src="/img/Badge-White.png" className="menu-desktop__logo" />
+        <div className={ `menu-mobile ${ props.isOpen ? 'menu-mobile--open' : 'menu-mobile--close' }` }>
+          <div className="menu-mobile__wrapper">
+            <div className="menu-mobile__header">
+              <img src="/img/Badge-White.png" className="menu-mobile__logo" />
               <a onClick={ props.handleToggle } >
-                <Icon name="bars" className="menu-desktop__toggle" onClick={ props.handleToggle } />
+                <Icon name="bars" className="menu-mobile__toggle" onClick={ props.handleToggle } />
               </a>
             </div>
-            <p className="menu-desktop__title">MENU</p>
-              <Link className="menu-desktop__item" to="/">
-                <img className="menu-desktop__icon" src="/img/Home-icon@2x.png" />
-                <span className="menu-desktop__item-label" >Overview</span>
+            <p className="menu-mobile__title">MENU</p>
+              <Link className="menu-mobile__item" to="/">
+                <img className="menu-mobile__icon" src="/img/Home-icon@2x.png" />
+                <span className="menu-mobile__item-label" >Overview</span>
               </Link>
-              <Link className="menu-desktop__item" to="/movement">
-                <img className="menu-desktop__icon" src="/img/Analytics-icon@2x.png" />
-                <span className="menu-desktop__item-label" >Movement</span>
-              </Link>
-              {/*
-              <Link className="menu-desktop__item" to="/top">
-                <img className="menu-desktop__icon" src="/img/Ratings-icon@2x.png" />
-                <span className="menu-desktop__item-label">Top 100</span>
-              </Link>
-              */}
-              <Link className="menu-desktop__item" to="/masternode">
-                <img className="menu-desktop__icon" src="/img/Customers-icon@2x.png" />
-                <span className="menu-desktop__item-label">Masternode</span>
-              </Link>
-              <Link className="menu-desktop__item" to="/coin">
-                <img className="menu-desktop__icon" src="/img/Market-icon@2x.png" />
-                <span className="menu-desktop__item-label" >Coin Info</span>
+              <Link className="menu-mobile__item" to="/movement">
+                <img className="menu-mobile__icon" src="/img/Analytics-icon@2x.png" />
+                <span className="menu-mobile__item-label" >Movement</span>
               </Link>
               {/*
-              <Link className="menu-desktop__item" to="/faq">
-                <img className="menu-desktop__icon" src="/img/Jobs-icon@2x.png" />
-                <span className="menu-desktop__item-label">FAQ</span>
+              <Link className="menu-mobile__item" to="/top">
+                <img className="menu-mobile__icon" src="/img/Ratings-icon@2x.png" />
+                <span className="menu-mobile__item-label">Top 100</span>
               </Link>
               */}
-              <Link className="menu-desktop__item" to="/api">
-                <img className="menu-desktop__icon" src="/img/Support-icon@2x.png" />
-                <span className="menu-desktop__item-label">API</span>
+              <Link className="menu-mobile__item" to="/masternode">
+                <img className="menu-mobile__icon" src="/img/Customers-icon@2x.png" />
+                <span className="menu-mobile__item-label">Masternode</span>
+              </Link>
+              <Link className="menu-mobile__item" to="/coin">
+                <img className="menu-mobile__icon" src="/img/Market-icon@2x.png" />
+                <span className="menu-mobile__item-label" >Coin Info</span>
+              </Link>
+              {/*
+              <Link className="menu-mobile__item" to="/faq">
+                <img className="menu-mobile__icon" src="/img/Jobs-icon@2x.png" />
+                <span className="menu-mobile__item-label">FAQ</span>
+              </Link>
+              */}
+              <Link className="menu-mobile__item" to="/api">
+                <img className="menu-mobile__icon" src="/img/Support-icon@2x.png" />
+                <span className="menu-mobile__item-label">API</span>
               </Link>
             </div>
         </div>
