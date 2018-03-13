cc6440e27c11a81a08b1519f6e46a218b3ac69ca
Test <test@email.com>
2018-03-13 16:15:15 -0700

    added page loading indicator

--- client/component/Loading.jsx
+++ client/component/Loading.jsx
@@ -7,7 +7,7 @@ import React from 'react';
 const Loading = () => (
   <div className="loading">
     <div className="content">
-      Loading stuff...
+      <div class="timer" />
     </div>
   </div>
 );
