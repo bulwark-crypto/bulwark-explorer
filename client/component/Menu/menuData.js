c6d11e201df5c8aadaba1901e856737d49358103
Unknown <padraiq@gmail.com>
2018-03-13 17:02:33 +0000

    Add peers menu item & fix mobile menu link color

--- client/component/Menu/menuData.js
+++ client/component/Menu/menuData.js
@@ -3,6 +3,7 @@ const MenuData = [
   {label: 'Movement', icon: '/img/movement.svg', href: '/movement'},
   // {label: 'Top 100', icon: '/img/top100.svg', href: '/top'}
   {label: 'Masternode', icon: '/img/masternodes.svg', href: '/masternode'},
+  {label: 'Connections', icon: '/img/connections.svg', href: '/peer'},
   {label: 'Coin Info', icon: '/img/coininfo.svg', href: '/coin'},
   {label: 'Support', icon: '/img/api.svg', href: '/api'}
 ]
