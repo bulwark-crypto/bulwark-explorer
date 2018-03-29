132be718e6d5032f34e18a1a4687a7a007aa7510
Dustin Engle <dustin@dustinengle.com>
2018-03-28 18:26:36 -0700

    updated route params

--- server/route/ext.js
+++ server/route/ext.js
@@ -7,8 +7,8 @@ const router = express.Router();
 // Iquidus Explorer routes.
 router.get('/getmoneysupply', iquidus.getmoneysupply);
 router.get('/getdistribution', iquidus.getdistribution);
-router.get('/getaddress/:address', iquidus.getaddress);
-router.get('/getbalance/:address', iquidus.getbalance);
+router.get('/getaddress/:hash', iquidus.getaddress);
+router.get('/getbalance/:hash', iquidus.getbalance);
 router.get('/getlasttxs', iquidus.getlasttxs);
 
 module.exports =  router;
