880ffd33f1cb962954b4f0d1e63436fbf12ffb23
dustinengle <dustin.engle@gmail.com>
2018-04-25 11:03:57 -0700

    added index to utxo model

--- model/utxo.js
+++ model/utxo.js
@@ -10,7 +10,7 @@ const UTXO = mongoose.model('UTXO', new mongoose.Schema({
   __v: { select: false, type: Number },
   _id: { required: true, select: false, type: String },
   address: { required: true, type: String },
-  blockHeight: { required: true, type: Number },
+  blockHeight: { index: true, required: true, type: Number },
   n: { required: true, type: Number },
   txId: { required: true, type: String },
   value: { required: true, type: Number }
