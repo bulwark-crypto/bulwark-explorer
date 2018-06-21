a46cf99ac529612ec1374acaa90ae42323aa5f5d
dustinengle <dustin.engle@gmail.com>
2018-06-20 23:03:36 -0700

    adding cluster support

--- server/index.js
+++ server/index.js
@@ -1,28 +1,45 @@
 
 require('babel-polyfill');
-const config = require('../config');
-const db = require('../lib/db');
-const express = require('express');
-const mongoose = require('mongoose');
-// Application.
-const middleware = require('./lib/middleware');
-const router = require('./lib/router');
+const cluster = require('cluster');
 
-/* Database */
-// Connect to the database.
-mongoose.connect(db.getDSN(), db.getOptions());
+// Master
+if (cluster.isMaster) {
+  const cpus = require('os').cpus().length;
 
-/* API */
-// Setup the application.
-const app = express();
-// Setup middleware for app.
-middleware(app);
-// Setup the routes.
-router(app);
-// Start the server.
-app.listen(config.api.port, () => {
-  console.log(`BlocEx running on port ${ config.api.port }`);
-});
+  for (let i = 0; i < cpus; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker) => {
+    cluster.fork();
+  });
+}
+// Worker
+else {
+  const config = require('../config');
+  const db = require('../lib/db');
+  const express = require('express');
+  const mongoose = require('mongoose');
+  // Application.
+  const middleware = require('./lib/middleware');
+  const router = require('./lib/router');
+
+  /* Database */
+  // Connect to the database.
+  mongoose.connect(db.getDSN(), db.getOptions());
+
+  /* API */
+  // Setup the application.
+  const app = express();
+  // Setup middleware for app.
+  middleware(app);
+  // Setup the routes.
+  router(app);
+  // Start the server.
+  app.listen(config.api.port, () => {
+    console.log(`BlocEx running on port ${ config.api.port }`);
+  });
+}
 
 // Export for testing.
 module.exports =  app;
