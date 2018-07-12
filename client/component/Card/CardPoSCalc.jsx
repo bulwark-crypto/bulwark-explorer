4caf6fe8dc65f12d10f727048cfcff731a5f7385
dustinengle <dustin.engle@gmail.com>
2018-07-12 13:20:31 -0700

    changed calc layout

--- client/component/Card/CardPoSCalc.jsx
+++ client/component/Card/CardPoSCalc.jsx
@@ -6,44 +6,41 @@ import Card from './Card';
 import Icon from '../Icon';
 
 export default class CardPoSCalc extends React.Component {
-    constructor(props) {
-        super(props);
-        this.input = null;
-    };
+  constructor(props) {
+    super(props);
+    this.input = null;
+  };
 
-    handleClick = (ev) => {
-        const v = this.input.value;
-        console.log('PoS Calc:', v);
+  handleClick = (ev) => {
+    const v = this.input.value;
 
-        if (!!v && !isNaN(v)) {
-            document.location.href = `/#/pos/${ v }`;
-        }
-    };
+    if (!!v && !isNaN(v)) {
+      document.location.href = `/#/pos/${ v }`;
+    }
+  };
 
-    render() {
-        return (
-            <Card title="PoS Calculator">
-                <div className="row">
-                    <div className="col-sm-12">
-                        <input
-                          onClick={ this.handleClick }
-                          ref={ i => this.input = i }
-                          type="text" />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-sm-12">
-                        Note: the following calculations are an estimation.
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-sm-12">
-                        <button onClick={ this.handleClick }>
-                          Estimate
-                        </button>
-                    </div>
-                </div>
-            </Card>
-        );
-    };
+  render() {
+    return (
+      <Card title="PoS Calculator">
+        <div className="row">
+          <div className="col-sm-12 col-md-8">
+            <input
+              onClick={ this.handleClick }
+              ref={ i => this.input = i }
+              type="text" />
+          </div>
+          <div className="col-sm-12 col-md-4">
+            <button onClick={ this.handleClick }>
+              Estimate
+            </button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-12">
+            Something here about how it is only an estimate.
+          </div>
+        </div>
+      </Card>
+    );
+  };
 }
