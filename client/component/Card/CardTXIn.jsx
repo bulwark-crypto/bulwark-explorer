57b8e6a6db6931ca41b8f3a947ae8f7cfc7920f8
Hodlforjesus <50343356+hodlforjesus@users.noreply.github.com>
2019-06-02 12:01:03 -0400

    Added Age & confirmations to TXin table

--- client/component/Card/CardTXIn.jsx
+++ client/component/Card/CardTXIn.jsx
@@ -25,6 +25,17 @@ export default class CardTXIn extends Component {
         { key: 'value', title: 'Value' }
       ]
     };
+
+    // If any inputs have a related vout add some extra columns. 
+    // Ex: Coinbase transactions would not have relatedVout so we don't need to show a bunch of empty data
+    if (this.props.txs.some(tx => tx.relatedVout)) {
+      this.state.cols = [
+        { key: 'address', title: 'Address' },
+        { key: 'age', title: 'Age' },
+        { key: 'confirmations', title: 'Confirmations' },
+        { key: 'value', title: 'Value' }
+      ]
+    }
   };
 
   render() {
@@ -33,9 +44,15 @@ export default class CardTXIn extends Component {
         cols={ this.state.cols }
         data={ this.props.txs.map(tx => ({
           ...tx,
-          address: tx.relatedVout && tx.relatedVout.address
+          address: tx.relatedVout
             ? (<Link to={ `/address/${ tx.relatedVout.address }` }>{ tx.relatedVout.address }</Link>)
             : tx.coinbase ? 'COINBASE' : 'Unknown',
+          age: tx.relatedVout
+            ? (<Link to={ `/address/${ tx.relatedVout.address }` }>{ (tx.relatedVout.age / 60 / 60).toFixed(2) }h</Link>)
+            : '',
+          confirmations: tx.relatedVout
+            ? (<Link to={ `/address/${ tx.relatedVout.address }` }>{ tx.relatedVout.confirmations }</Link>)
+            : '',
           value: tx.relatedVout
             ? (
                 <span className="badge badge-danger">
