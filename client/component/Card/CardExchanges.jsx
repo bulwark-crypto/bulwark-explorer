06ba3df4dfa97f51e7d368b3fba913dedc7be087
LyGhT <37241762+LyGhT1337@users.noreply.github.com>
2019-06-05 18:09:39 +0200

    Update CardExchanges.jsx

--- client/component/Card/CardExchanges.jsx
+++ client/component/Card/CardExchanges.jsx
@@ -5,10 +5,12 @@ import Card from './Card';
 
 const CardExchanges = () => (
   <Card title="Exchanges">
+    <a href="https://www.altilly.com/market/BWK_BTC" target="_blank">altilly</a><br />
     <a href="https://bastionex.com/" target="_blank">Bastion</a><br />
-    <a href="https://crypto-bridge.org/" target="_blank">CryptoBridge</a><br />
-    <a href="https://txbit.io/Trade/BWK/BTC" target="_blank">Txbit.io</a><br />
     <a href="https://blocknet.co/" target="_blank">Blocknet</a><br />
+    <a href="https://crypto-bridge.org/" target="_blank">CryptoBridge</a><br />
+    <a href="https://novaexchange.com/market/BTC_BWK/" target="_blank">NovaExchange</a><br />
+    <a href="https://txbit.io/Trade/BWK/BTC" target="_blank">Txbit.io</a><br />
   </Card>
 );
 
