6973f076ef58fb618d51a2fdbcb398376749a050
Hodlforjesus <50343356+hodlforjesus@users.noreply.github.com>
2019-06-05 20:08:12 -0400

    Fixed doubling of reward during sync, we no longer need to clean on sync

--- cron/block.js
+++ cron/block.js
@@ -12,6 +12,7 @@ const util = require('./util');
 const Block = require('../model/block');
 const TX = require('../model/tx');
 const UTXO = require('../model/utxo');
+const BlockRewardDetails = require('../model/blockRewardDetails');
 
 /**
  * Process the blocks and transactions.
@@ -23,6 +24,7 @@ async function syncBlocks(start, stop, clean = false) {
     await Block.remove({ height: { $gte: start, $lte: stop } });
     await TX.remove({ blockHeight: { $gte: start, $lte: stop } });
     await UTXO.remove({ blockHeight: { $gte: start, $lte: stop } });
+    await BlockRewardDetails.remove({ blockHeight: { $gte: start, $lte: stop } });
   }
 
   let block;
@@ -138,7 +140,7 @@ async function update() {
     const info = await rpc.call('getinfo');
     const block = await Block.findOne().sort({ height: -1 });
 
-    let clean = true; // Always clear for now.
+    let clean = false; // We no longer need to clean by default because block is the last item inserted. If we have the block that means all data for that block exists
     let dbHeight = block && block.height ? block.height : 1;
     let rpcHeight = info.blocks;
 
