5fc2e8dd118708429404d1400ecd87cda1053d9a
LyGhT <37241762+LyGhT1337@users.noreply.github.com>
2019-06-14 22:13:59 +0200

    Update CardExchanges.jsx

--- client/component/Card/CardExchanges.jsx
+++ client/component/Card/CardExchanges.jsx
@@ -9,6 +9,7 @@ const CardExchanges = () => (
     <a href="https://bastionex.com/" target="_blank">Bastion</a><br />
     <a href="https://blocknet.co/" target="_blank">Blocknet</a><br />
     <a href="https://crypto-bridge.org/" target="_blank">CryptoBridge</a><br />
+    <a href="https://green-dex.net/market/GREENDEX.BWK_GREENDEX.BTC" target="_blank">GreenDex</a><br />
     <a href="https://novaexchange.com/market/BTC_BWK/" target="_blank">NovaExchange</a><br />
     <a href="https://txbit.io/Trade/BWK/BTC" target="_blank">Txbit.io</a><br />
   </Card>
