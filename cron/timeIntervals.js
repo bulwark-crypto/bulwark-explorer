f12a30ca9d2a05024aec6a5bca518f58aeb4f2ae
Hodlforjesus <50343356+hodlforjesus@users.noreply.github.com>
2019-09-29 15:57:45 -0400

    Testing syncing time intervals

--- cron/timeIntervals.js
+++ cron/timeIntervals.js
@@ -44,6 +44,7 @@ const syncTimeIntervals = async () => {
 
     var blockRewardDetailsCursor = timeIntervalSettings.model.aggregate(aggregationPipeline).cursor().exec();
 
+    /*
     await blockRewardDetailsCursor.eachAsync(async (item) => {
       const intervalNumber = moment(item._id, 'YYYY-MM-DD').utc().unix();
 
@@ -56,11 +57,12 @@ const syncTimeIntervals = async () => {
       await newTimeIntervalItem.save();
     })
   }
+  */
 
-  /*
+
     await syncTimeIntervalSettings({
       type: TimeIntervalType.DailyAvgPosRoi,
-  
+
       model: BlockRewardDetails,
       aggregationPipeline: [
         { $match: { 'stake': { $exists: true } } }, //@todo would be really cool if we could identify if stake exists on block reward Model via a bool?
@@ -68,66 +70,66 @@ const syncTimeIntervals = async () => {
         { $group: { _id: '$value', value: { $avg: '$stake.roi' } } },
         { $sort: { _id: -1 } },
       ]
-    });*/
-
-  /*
-  await syncTimeIntervalSettings({
-    type: TimeIntervalType.DailyNonRewardTransactionsCount,
-
-    model: CarverMovement,
-    aggregationPipeline: [
-      { $match: { isReward: false } },
-      { $project: { yearMonthDay: { $dateToString: { format: "%Y-%m-%d", date: "$date" } } } },
-      { $group: { _id: '$yearMonthDay', value: { $sum: 1 } } },
-      { $sort: { _id: -1 } }
-    ]
-  });*/
-
-  await syncTimeIntervalSettings({
-    type: TimeIntervalType.DailyAvgPosInputValue,
-
-    model: BlockRewardDetails,
-    aggregationPipeline: [
-      { $match: { 'stake': { $exists: true } } }, //@todo would be really cool if we could identify if stake exists on block reward Model via a bool?
-      { $project: { 'stake.input.value': 1, value: { $dateToString: { format: '%Y-%m-%d', date: '$date' } } } },
-      { $group: { _id: '$value', value: { $avg: '$stake.input.value' } } },
-      { $sort: { _id: -1 } },
-    ]
-  });
-
-  //@todo abg stake.ageTime
-
-  //@todo avg tx input (non-reward)
-  //db.carverMovements.aggregate([ {$match:{isReward:false}}, {$project:{amountIn:1,yearMonthDay: { $dateToString: { format: "%Y-%m-%d", date: "$date" } } }}, {$group:{_id:'$yearMonthDay',value:{$avg:'$amountIn'}}} ,{$sort:{_id:-1}}])
+    });
 
 
+    await syncTimeIntervalSettings({
+      type: TimeIntervalType.DailyNonRewardTransactionsCount,
 
-  console.log('Syncing complete');
-}
+      model: CarverMovement,
+      aggregationPipeline: [
+        { $match: { isReward: false } },
+        { $project: { yearMonthDay: { $dateToString: { format: "%Y-%m-%d", date: "$date" } } } },
+        { $group: { _id: '$yearMonthDay', value: { $sum: 1 } } },
+        { $sort: { _id: -1 } }
+      ]
+    });
+
+    await syncTimeIntervalSettings({
+      type: TimeIntervalType.DailyAvgPosInputValue,
+
+      model: BlockRewardDetails,
+      aggregationPipeline: [
+        { $match: { 'stake': { $exists: true } } }, //@todo would be really cool if we could identify if stake exists on block reward Model via a bool?
+        { $project: { 'stake.input.value': 1, value: { $dateToString: { format: '%Y-%m-%d', date: '$date' } } } },
+        { $group: { _id: '$value', value: { $avg: '$stake.input.value' } } },
+        { $sort: { _id: -1 } },
+      ]
+    });
+
+    //@todo abg stake.ageTime
+
+    //@todo avg tx input (non-reward)
+    //db.carverMovements.aggregate([ {$match:{isReward:false}}, {$project:{amountIn:1,yearMonthDay: { $dateToString: { format: "%Y-%m-%d", date: "$date" } } }}, {$group:{_id:'$yearMonthDay',value:{$avg:'$amountIn'}}} ,{$sort:{_id:-1}}])
 
 
-/**
- * Handle locking.
- */
-async function update() {
-  const type = 'timeIntervals';
-  let code = 0;
 
-  try {
-    locker.lock(type);
-    await syncTimeIntervals();
-  } catch (err) {
-    console.log(err);
-    code = 1;
-  } finally {
+    console.log('Syncing complete');
+  }
+
+
+  /**
+   * Handle locking.
+   */
+  async function update() {
+    const type = 'timeIntervals';
+    let code = 0;
+
     try {
-      locker.unlock(type);
+      locker.lock(type);
+      await syncTimeIntervals();
     } catch (err) {
       console.log(err);
       code = 1;
+    } finally {
+      try {
+        locker.unlock(type);
+      } catch (err) {
+        console.log(err);
+        code = 1;
+      }
+      exit(code);
     }
-    exit(code);
   }
-}
 
-update();
+  update();
