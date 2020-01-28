f6290c9dd0c5fcff77b575801dec6043f50175e4
Hodlforjesus <50343356+hodlforjesus@users.noreply.github.com>
2020-01-27 19:42:22 -0500

    Added crex24 exchange

--- client/component/Card/CardExchanges.jsx
+++ client/component/Card/CardExchanges.jsx
@@ -7,6 +7,7 @@ const CardExchanges = () => (
   //@todo move this to config to avoid conflicts
   <Card title="Exchanges">
     <a href="https://txbit.io/Trade/BWK/BTC" target="_blank" rel="nofollow noopener">Txbit.io</a><br />
+    <a href="https://crex24.com/exchange/BWK-BTC" target="_blank" rel="nofollow noopener">Crex24</a><br />
     <a href="https://graviex.net/markets/bwkbtc/" target="_blank" rel="nofollow noopener">Graviex</a><br />
     <a href="https://bastionex.com/" target="_blank" rel="nofollow noopener">Bastion</a><br />
     <a href="https://blocknet.co/" target="_blank" rel="nofollow noopener">Blocknet</a><br />
